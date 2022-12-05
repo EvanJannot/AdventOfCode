48d4c2933f82cfc2ea351d7a2fc49e5c898b1b18
JANNOT <evan.jannot@sogeti.com>
2022-12-05 19:41:37 +0100

    Upgrade Day5 answer to automatically check the number of columns and lines

--- Day5/day5.py
+++ Day5/day5.py
@@ -1,18 +1,36 @@
 input = open("Day5/input.txt","r")
-stacks = [[],[],[],[],[],[],[],[],[]]
+
+# Count the number of characacters of the first line
+# This is the number of columns
+columns = len(input.readline())
+# Reset the file pointer to the beginning of the file
+input.seek(0)
+stacks = [[] for i in range(columns//4)]
 
 countLine = 1
 for line in input :
-    if countLine <=8 : 
-        stacks[0].append(line[1])
-        stacks[1].append(line[5])
-        stacks[2].append(line[9])
-        stacks[3].append(line[13])
-        stacks[4].append(line[17])
-        stacks[5].append(line[21])
-        stacks[6].append(line[25])
-        stacks[7].append(line[29])
-        stacks[8].append(line[33])
+    if line[0] == 'm': 
+        if len(line) <= 17 :
+            break
+        else : 
+            numberToMove = int(line[5]+line[6])
+            whereItIs = int(line[12]+line[13])
+            if len(line)>18: 
+                whereToMove = int(line[17]+line[18])
+            else : 
+                whereToMove = int(line[17])
+            supplies = []
+            for i in range (0, numberToMove):
+                supplies.append(stacks[whereItIs-1][i])
+            for element in supplies : 
+                stacks[whereItIs-1].remove(element)
+            supplies.reverse()
+            stacks[whereToMove-1] = supplies + stacks[whereToMove-1]
+            supplies.clear()
+    elif line.count('[') :  
+        for i in range (0,columns) :
+            if i%4 == 1 :
+                stacks[((i)//4)].append(line[i]) 
         for stack in stacks:
             count = 0
             for element in stack :
@@ -20,19 +38,10 @@ for line in input :
                     count+=1
             for i in range (0, count):
                 stack.remove(' ')
-    elif countLine > 10 : 
-        numberToMove = int(line[5]+line[6])
-        whereItIs = int(line[12]+line[13])
-        whereToMove = int(line[17]+line[18])
-        supplies = []
-        for i in range (0, numberToMove):
-            supplies.append(stacks[whereItIs-1][i])
-        for element in supplies : 
-            stacks[whereItIs-1].remove(element)
-        supplies.reverse()
-        stacks[whereToMove-1] = supplies + stacks[whereToMove-1]
-        supplies.clear()
+    else : 
+        pass
     countLine += 1
+    
 
 
 print(stacks)
